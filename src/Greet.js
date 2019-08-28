18a9524d0ee9bf039f55c60e1771dd2b123e3d50
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-08-28 22:45:18 +0530

    "Basic component type"

---
+++ src/Greet.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// with the Default Rfc short cut
+// export default function Greet() {
+//     return (
+//         <div>
+//             Harsha
+//         </div>
+//     )
+// }
+
+// with the JS function
+// function Greet(){
+//      return <h2>Harsha</h2>
+// }
+//export default Greet;
+
+// Es6 Arrow function
+const Greet = () => { return <h2>Harsha</h2> }
+export default Greet
