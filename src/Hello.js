553fe138a79240159239aa6923b649140d99c805
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-09-12 15:43:34 +0530

    "set state and state"

--- src/Hello.js
+++ src/Hello.js
@@ -11,3 +11,4 @@ function Hello(props){
 
 
 export default Hello;
+
--- src/welcome.js
+++ src/welcome.js
@@ -1,4 +1,4 @@
-import React , { Component } from "react";
+import React, { Component } from "react";
 
 // class component
 // class Welcome extends Component{
@@ -11,14 +11,91 @@ import React , { Component } from "react";
 // }
 
 // props with class component
-class Welcome extends Component{
+// class Welcome extends Component{
+//     render(){
+//         return(
+//             <div>
+//             <h1>Welcome { this.props.name } { this.props.marks}</h1>
+//         </div>
+//         )
+//     }
+// }
+
+// export default Welcome;
+
+// class component with this
+// class India extends Component{
+//   gitName(){
+//     return "Class component with this"
+//   }
+//   render(){
+//     return(
+//       <div>
+//          <h1>Helllloooo</h1>
+//          { this.gitName() }
+//       </div>
+//     )
+//   }
+// }
+// export default India;
+
+// class component with state count example
+// class India extends Component{
+//     constructor(){
+//         super();
+//        this.state = {
+//           count:0
+//        } 
+
+//        this.incrementconurt = this.incrementconurt.bind(this);
+//     }
+
+//     incrementconurt(){
+//         let count = this.state.count;
+//             this.setState(
+//                 {
+//                     count: count + 1
+//                 }
+//             )
+//     }
+
+//   render() {
+//     return(
+//     <div>harsha new 
+//     { this.state.count }
+//     <button onClick = { this.incrementconurt }>Add counter</button>
+//     </div>
+//     ) 
+//   }
+// }
+
+// export default India;
+
+
+class Example extends Component{
+    constructor(){
+        super();
+        this.state = {
+            btntext:"Subcribe here"
+        }
+    }
+
+    changebtntext(){
+        this.setState({
+            btntext:"thnakyou"
+        })
+    }
+
+    
     render(){
         return(
             <div>
-            <h1>Welcome { this.props.name } { this.props.marks}</h1>
-        </div>
-        ) 
+                <h1>{ this.state.btntext }</h1>
+                <button onClick={() => this.changebtntext() }>CLick here</button>
+                </div>        
+        )
     }
 }
 
-export default Welcome;
+export default Example;
+
